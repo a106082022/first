3f7bba341625a8034c838b84663b8be4e0853c1b
DESKTOP-NU25MII\grass <grass527@gmail.com>
2018-05-09 13:31:15 +0800

    first submit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicWpfNotepad
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filePath = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // 新開檔案按鈕
+        private void NewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            filePath = "";
+            TextArea.Text = "";
+        }
+
+
+        // 開啟檔案按鈕
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // 產生開啟檔案視窗 OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // 顯示視窗
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // 當按下開啟之後的反應 
+            if (result == true)
+            {
+                // 取得檔案路徑 
+                filePath = dlg.FileName;
+
+                // 讀取檔案
+                TextArea.Text = System.IO.File.ReadAllText(filePath);
+            }
+        }
+
+
+        // 存檔檔案按鈕
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (filePath != "")
+            {
+                System.IO.File.WriteAllText(filePath, TextArea.Text);
+            }
+            else
+            {
+                SaveAs();
+            }
+        }
+
+
+        // 另存檔案按鈕
+        private void SaveAsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveAs();
+        }
+
+
+        // 另存檔案
+        private void SaveAs()
+        {
+            // 產生開啟檔案視窗 OpenFileDialog 
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+            // 顯示視窗
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // 當按下開啟之後的反應 
+            if (result == true)
+            {
+                // 取得檔案路徑 
+                filePath = dlg.FileName;
+
+                // 儲存檔案
+                System.IO.File.WriteAllText(filePath, TextArea.Text);
+            }
+        }
+    }
+}
